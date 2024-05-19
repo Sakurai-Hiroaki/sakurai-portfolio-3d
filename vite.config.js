08b8b38fdfd76f5550b76c52d22a25af4c51d491
kaityuu-21 <kaityuu39@gmail.com>
2024-05-19 20:14:26 +0900

    ロールアップオプションの追加

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,9 @@ export default {
     },
     build:
     {
+        rollupOptions: {
+            external: ['react-swipeable']
+          },
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
